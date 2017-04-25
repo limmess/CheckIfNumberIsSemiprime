e5c7b8bcc565989e82af8089b558786feae2a373
limmess <limmes@yahoo.com>
2017-04-25 22:32:28 +0300

    Add project files.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CheckIfNumberIsSemiprime
+{
+    public class Jou
+    {
+        //check if number is semiprime
+        static public bool Run(int number)
+        {
+            int a = 2, b = 0;
+            while (b < 3 && number != 1)
+            {
+                if ((number % a) == 0)
+                { number /= a; b++; }
+                else a++;
+            }
+
+            bool isSemiprime = b == 2;
+            return isSemiprime;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Jou jjj = new Jou();
+
+            Console.WriteLine(Jou.Run(2));
+            Console.ReadKey();
+        }
+    }
+}
